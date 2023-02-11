aac507de17e3618596c59fdc71799d03f4a431c3
Mark Kim <mark340@naver.com>
2023-02-11 18:37:12 +0900

    Add: Logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+
+  await app.listen(port);
+  console.log(`🔰listening on port ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
