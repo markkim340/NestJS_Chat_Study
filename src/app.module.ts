cda58a18513bc64aae47348747cc4c0344e20543
Mark Kim <mark340@naver.com>
2023-02-11 19:01:35 +0900

    Add: Users CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { JoinRequestDto } from './dto/join.request.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  postUsers(email: string, nickname: string, password: string) {
+    return data;
+  }
+}
